76bc7e65596b908f5b13ca0845b2b21ce17d0be5
OlegV <o.vitvitskyi@gmail.com>
2015-01-10 17:30:23 +0200

    Added basic templating

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,28 @@
  */
 angular.module('firebaseAngularDemoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    var author = 'Me';
+    
+    $scope.messages = [
+		{text: 'Message 1', datestamp: new Date(), author: 'Misko'},
+		{text: 'Message from Yo', datestamp: new Date(), author: 'Tom'}
     ];
+
+    $scope.users = [
+    	{username: 'Misko', img:''},
+    	{username: 'Vojta', img:''},
+    	{username: 'Tom', img:''},		
+    ];
+
+    $scope.addMessage = function(){
+    	if (!$scope.newMessage) { return; }
+    	$scope.messages.push(
+    		{	
+    			datestamp : new Date(),
+    			author : author,
+    			text : $scope.newMessage
+    		}
+    	);
+    	$scope.newMessage = '';
+    };
   });
