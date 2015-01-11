8e9462947ff496a86c76448342c633df8bba11ad
OlegV <o.vitvitskyi@gmail.com>
2015-01-11 12:05:09 +0200

    Added users ref

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,29 +8,58 @@
  * Controller of the firebaseAngularDemoApp
  */
 angular.module('firebaseAngularDemoApp')
-  .controller('MainCtrl', [ '$scope', '$firebase', function ($scope, $firebase) {
-    var author = 'Me';
+  .controller('MainCtrl', [ '$scope', '$firebase', 
+  	function ($scope, $firebase) {
+		
     var refMessages = new Firebase('https://crackling-fire-2244.firebaseio.com/messages');
+    var refUsers = new Firebase('https://crackling-fire-2244.firebaseio.com/users');
+    var currentUser;
+
     var syncMessages = $firebase(refMessages);
+    var syncUsers = $firebase(refUsers);
 
     $scope.messages = syncMessages.$asArray();
+    $scope.users = syncUsers.$asArray();
 
-    $scope.users = [
-    	{username: 'Misko', img:''},
-    	{username: 'Vojta', img:''},
-    	{username: 'Tom', img:''},		
-    ];
+    $scope.saveUser = function(){
+			if (currentUser) {
+				updateUser(currentUser, $scope.author);
+			} else {
+				currentUser = createUser($scope.author);
+				$scope.currUserId = currentUser.key();
+			}
+    };
+
+    $scope.isCurrentUser = function(id){
+    	return id === $scope.currUserId;
+    };
 
     $scope.addMessage = function(text){
     	if (!$scope.newMessage) { return; }
+    	if (!$scope.author) { window.alert('Enter user name!'); return;}
+    	var datestamp = new Date();
     	$scope.messages.$add(
     		{	
-    			datestamp : new Date(),
-    			author : author,
+    			datestamp : datestamp.valueOf(),
+    			author : $scope.author,
     			text : text
     		}
     	);
     	$scope.newMessage = '';
     };
+
+    $scope.keyDownMsg = function(e){
+    	if (e.keyCode === 13 && $scope.newMessage) {
+    		$scope.addMessage($scope.newMessage);
+    	}
+  	};
+
+  	function createUser (name){
+    	return refUsers.push({username: name});
+    }
+
+    function updateUser (userRef, name) {
+    	userRef.update({username : name });
+    }
 
   }]);
